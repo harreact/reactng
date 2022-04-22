66ee04b6a38a1237516caaba049672d3f1ef17ef
Hemavathi <hemavathi08.gajjala@gmail.com>
2022-04-22 21:12:14 +0530

    Initial commit using git CLI from windows

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Hasha world
+          Welcome to Harsha's world
         </p>
         <a
           className="App-link"
